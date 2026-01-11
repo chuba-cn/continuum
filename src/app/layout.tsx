4e7d8d082e3a83241c66f4708af2867a5ea3125a
Chuba . C <chinemelumchubanwene57@gmail.com>
2026-01-11 02:25:12 +0100

    02b-[feat/auth] - Add and setup authetication with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import { Nunito_Sans, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { dark } from "@clerk/themes"; 
 
 const nunitoSans = Nunito_Sans({ variable: "--font-sans" });
 
@@ -22,14 +31,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${nunitoSans.variable} ${plexMono.variable} antialiased`}
-      >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        theme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${nunitoSans.variable} ${plexMono.variable} antialiased`}
+        >
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <header>
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-primary text-primary-foreground">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
